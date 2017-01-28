b8dbc2ae2a75375faeb8b80695ea38a2c828f8d2
Paul WB <higgsbison@gmail.com>
2017-01-28 14:39:08 -0800

    build sign in form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -4,7 +4,19 @@ import { reduxForm } from 'redux-form';
 class Signin extends Component {
   render() {
     return (
-      
+        <form>
+          <fieldset className="form-group">
+            <label>Email:</label>
+            <input className="form-control" />
+          </fieldset>
+          <fieldset className="form-group">
+            <label>Password:</label>
+            <input className="form-control" />
+          </fieldset>
+          <button action="submit" className="btn btn-primary">
+            Sign In
+          </button>
+        </form>
     );
   }
 }
