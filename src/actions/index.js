796e4b98b0c2e412579d55f319e62a1ca802966f
Paul WB <higgsbison@gmail.com>
2017-01-28 19:06:19 -0800

    add auth reducer w auth and unauth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,27 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+import { AUTH_USER, UNAUTH_USER } from './types';
+
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   //submit email and password to server
   return (dispatch) => {
-    axios.post(`${ROOT_URL}/signin`, { email, password });
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+    .then((response) => {
+      // if request is good,
+      // * update state to indicate user is authenticated
+      dispatch({ type: AUTH_USER });
+
+      // * save jwt token
+
+      // * redirect to authorized section
+      browserHistory.push('/feature');
+    })
+    .catch(()=> {
+
+    });
   }
   // if request is good,
   // * update state to indicate user is authenticated
