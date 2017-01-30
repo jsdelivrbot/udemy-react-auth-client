e0c53cb00688e48e109ab57a9ba5406f2d02e275
Paul WB <higgsbison@gmail.com>
2017-01-29 16:59:35 -0800

    add signup form

---
+++ src/components/auth/signup.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+  render() {
+    const { handleSUbmit, fields: { email, password, passwordConfirm} } = this.props
+    return (
+      <form>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input className="form-control" {...email} />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password</label>
+          <input type="password" className="form-control" {...password} />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <input type="password" className="form-control" {...passwordConfirm} />
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign Up</button>
+      </form>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm'],
+}, null, null)(Signup);
--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,7 @@ class Header extends Component {
             <Link className="nav-link" to="/signin">Sign In</Link>
           </li>,
           <li className="nav-item" key={2}>
-            <Link className="nav-link" to="/signup">Sign Out</Link>
+            <Link className="nav-link" to="/signup">Sign Up</Link>
           </li>
         ];
       }
