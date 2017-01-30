3c2e15c6f77982b923b213eee8ddb468ebdb1690
Paul WB <higgsbison@gmail.com>
2017-01-29 20:27:29 -0800

    add require_auth component

---
+++ src/components/feature.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+  render() {
+    return (
+      <div>This is authenticated content</div>
+    )
+  }
+}
+
+export default Feature;
